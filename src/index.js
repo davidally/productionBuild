3143d538608a49fda8acf56070dc02daee27d6d8
david <davidally94@gmail.com>
2018-06-25 22:41:47 -0400

    production ready test kit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+import { has } from 'lodash';
+import './main.scss';
+
+console.log('Hello World');
+
+$(document).ready( function(){
+ 
+    let $standardSyntax = $('img');
+    const myFave = 'This is my favorite type of javascript!'; 
+});
