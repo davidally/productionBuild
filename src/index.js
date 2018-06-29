31ecb19a93011193a64142877b719680301f9e8d
david <davidally94@gmail.com>
2018-06-29 19:28:48 -0400

    checking package dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import './styles/main.scss';
 import $ from 'jquery';
-import { has } from 'lodash';
-import './main.scss';
+import _ from 'lodash';
 
 console.log('Hello World');
-
+ 
 $(document).ready( function(){
  
-    let $standardSyntax = $('img');
-    const myFave = 'This is my favorite type of javascript!'; 
-});
+    let $doesJQWork = $('img').attr('src');
+    const myFave = 'This is my favorite type of bundler!';
+    let newStr = [1,2,3,4,5,67,8,9];
+    let doesthiswork = _.drop(newStr, 3);
+    console.log(myFave);
+    console.log($doesJQWork);
+    console.log(doesthiswork);
+}); 
